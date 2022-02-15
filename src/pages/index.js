1e41479b58f3b6bfa6b5df1fd0e175fe0be7520b
edmundchan8 <pokedev8@gmail.com>
2022-02-14 16:12:14 -0800

    Finished Gatsby Tutorial part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 function index() {
   return (
     <Layout pageTitle="Edmund's Website">
+      <StaticImage
+      src="../images/Nightbonder.jpg"
+      alt="Nightbonder image"/>
       <p>I'm making a Gatsby Website for practice</p>
     </Layout>
   )
